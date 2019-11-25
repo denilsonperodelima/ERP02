e935483b0c3945c0ee1b0e03cadf1b5825292823
Pero <denilsonperodelima@gmail.com>
2019-11-25 19:27:53 -0300

    xxx

--- src/main/webapp/resources/lib/jquery/JUtil.js
+++ src/main/webapp/resources/lib/jquery/JUtil.js
@@ -2,6 +2,7 @@ function getAmbiente(){
 	    //return "http://localhost:8080";
         //return "http://35.225.125.57:8080";
 	    return "https://erp02.herokuapp.com";
+	    
 }
 
 function getUrlVars(){

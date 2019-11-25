47202049a7e81ca9c11db442ce57f934b4bb2f44
Pero <denilsonperodelima@gmail.com>
2019-11-25 19:47:37 -0300

    yyy

--- src/main/java/com/velocinotech/erp02/services/EmailService.java
+++ src/main/java/com/velocinotech/erp02/services/EmailService.java
@@ -3,8 +3,8 @@ package com.velocinotech.erp02.services;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
-import com.velocinotech.erp02.domain.Usuario;
 import com.velocinotech.erp02.domain.Pedido;
+import com.velocinotech.erp02.domain.Usuario;
 
 @Service
 public interface EmailService {

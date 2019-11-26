4ab09e85adbc986eff4b307272444a2975709e66
Pero <denilsonperodelima@gmail.com>
2019-11-26 08:52:49 -0300

    ssss

--- src/main/java/com/velocinotech/erp02/services/EmailService.java
+++ src/main/java/com/velocinotech/erp02/services/EmailService.java
@@ -1,12 +1,12 @@
 package com.velocinotech.erp02.services;
 
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.velocinotech.erp02.domain.Pedido;
 import com.velocinotech.erp02.domain.Usuario;
 
-@Component
+@Service
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
